0aa52269a6f295ad106f6821e1348b9e5e3931a2
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-08-29 15:37:14 +0530

    Make project with Appwrite and Tailwind configured

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div className="w-full text-center">Hello World!</div>
+  )
+}
+
+export default App
---
+++ src/constants/appwrite.ts
@@ -0,0 +1,7 @@
+export const DATABASE_ID: string = import.meta.env.VITE_APPWRITE_DATABASE_ID!;
+
+export const TABLE_USERS: string = import.meta.env.VITE_APPWRITE_TABLE_USERS!;
+export const TABLE_PORTFOLIOS: string = import.meta.env
+  .VITE_APPWRITE_TABLE_PORTFOLIOS!;
+export const TABLE_TEMPLATES: string = import.meta.env
+  .VITE_APPWRITE_TABLE_TEMPLATES!;
