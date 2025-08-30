0a0e8c2be32cf44076c2ae2babcfe9a54ac105d0
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-08-30 23:24:57 +0530

    Create signup and login page

---
+++ src/pages/loginPage.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "@/components/forms/loginForm"
+
+const LoginPage = () => {
+    return (
+        <LoginForm />
+    )
+}
+
+export default LoginPage
---
+++ src/pages/signupPage.tsx
@@ -0,0 +1,9 @@
+import SignupForm from "@/components/forms/signupForm"
+
+const SignupPage = () => {
+    return (
+        <SignupForm />
+    )
+}
+
+export default SignupPage
