83f5cb3d0c3a1f745fccd9059317259359364684
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-08-30 23:31:02 +0530

    Add persistant login functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { Toaster } from "sonner";
+import AppRoutes from "@/routes/routes";
+import { useDispatch, useSelector } from "react-redux";
+import type { RootState, AppDispatch } from "@/store/store";
+import { useEffect } from "react";
+import { getUserDataThunk } from "@/store/thunks/authThunk";
+
 function App() {
+  const dispatch = useDispatch<AppDispatch>();
+  const loadingType = useSelector((state: RootState) => state.auth.loadingType);
+  useEffect(() => {
+    dispatch(getUserDataThunk());
+  }, [dispatch]);
+
+  if (loadingType==="user") return <div>Loading...</div>;
   return (
-    <div className="w-full text-center">Hello World!</div>
-  )
+    <>
+      <Toaster position="top-right" richColors />
+      <AppRoutes />
+    </>
+  );
 }
 
-export default App
+export default App;
