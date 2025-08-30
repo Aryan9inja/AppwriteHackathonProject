8137c74ad8899a531b71f211a2886ab4c2839a70
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-08-30 23:28:54 +0530

    Add appwrite config file

--- src/lib/appwrite.config.ts
+++ src/lib/appwrite.config.ts
@@ -1,4 +1,4 @@
-import { Client, Account, Databases, Storage } from "appwrite";
+import { Client, Account, Storage, TablesDB } from "appwrite";
 
 const client = new Client();
 
@@ -7,5 +7,5 @@ client
   .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID!);
 
 export const account = new Account(client);
-export const databases = new Databases(client);
+export const tables = new TablesDB(client);
 export const storage = new Storage(client);
