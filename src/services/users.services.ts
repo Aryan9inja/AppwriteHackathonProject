e5c192372ccd02ca36d77360cb25b0413ae77b8d
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-08-30 23:22:13 +0530

    Add services using appwrite for users

---
+++ src/services/users.services.ts
@@ -0,0 +1,89 @@
+import { ID, Query } from "appwrite";
+import { account, tables } from "@/lib/appwrite.config";
+import type { LoginFormData, SignUpFormData, User, UserDoc } from "@/types/types";
+import { DATABASE_ID, TABLE_USERS } from "@/constants/appwrite";
+
+export const registerUser = async (formData: SignUpFormData) => {
+  try {
+    await account.create({
+      userId: ID.unique(),
+      email: formData.email,
+      password: formData.password,
+      name: formData.name,
+    });
+    await account.createEmailPasswordSession({
+      email: formData.email,
+      password: formData.password,
+    });
+    const user = await account.get();
+    return user;
+  } catch (error: any) {
+    console.log("SignUp Failed", error);
+    throw error;
+  }
+};
+
+export const loginUser = async (formData: LoginFormData) => {
+  try {
+    await account.createEmailPasswordSession({
+      email: formData.email,
+      password: formData.password,
+    });
+    const user = await account.get();
+    return user;
+  } catch (error: any) {
+    console.log("Login Failed", error);
+    throw error;
+  }
+};
+
+export const logout = async () => {
+  try {
+    await account.deleteSession({ sessionId: "current" });
+  } catch (error) {
+    console.log("Logout failed: ", error);
+    throw error;
+  }
+};
+
+
+export const getUserData = async () => {
+  try {
+    const accountUser = await account.get();
+
+    const docList = await tables.listRows<UserDoc>({
+      databaseId: DATABASE_ID,
+      tableId: TABLE_USERS,
+      queries: [Query.equal("userId", accountUser.$id)],
+    });
+
+    if (docList.rows.length === 0) {
+      const user: User = {
+        userId: accountUser.$id,
+        username: accountUser.email,
+        name: accountUser.name,
+      };
+
+      return {
+        user,
+        userDoc: null
+      };
+    }
+
+    const userDoc = docList.rows[0];
+
+    const user: User = {
+      userId: userDoc.userId,
+      username: userDoc.username,
+      name: userDoc.name,
+    };
+
+    return {
+      user,
+      userDoc
+    };
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    return null;
+  }
+};
