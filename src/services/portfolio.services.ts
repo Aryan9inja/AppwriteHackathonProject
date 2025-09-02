4ff934a1e52bb0bf68f059b124c6de9d92fd0a6f
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-09-02 22:50:20 +0530

    Refactor portfolio services to consolidate imports and add getPortfolioData function for fetching portfolio details

--- src/services/portfolio.services.ts
+++ src/services/portfolio.services.ts
@@ -1,5 +1,10 @@
-import { RESUME_BUCKET } from "@/constants/appwrite";
-import { storage } from "@/lib/appwrite.config";
+import {
+  DATABASE_ID,
+  RESUME_BUCKET,
+  TABLE_PORTFOLIOS,
+} from "@/constants/appwrite";
+import { storage, tables } from "@/lib/appwrite.config";
+import { PortfolioData, PortfolioDoc } from "@/types/types";
 import { ID } from "appwrite";
 
 export const uploadResume = async (file: File) => {
@@ -12,6 +17,24 @@ export const uploadResume = async (file: File) => {
     return uploadedResume.$id;
   } catch (error: unknown) {
     console.log("Resume Upload Failed", error);
+    throw error;
+  }
+};
+
+export const getPortfolioData = async (docId: string): Promise<PortfolioData> => {
+  try {
+    const doc: PortfolioDoc = await tables.getRow({
+      databaseId: DATABASE_ID,
+      tableId: TABLE_PORTFOLIOS,
+      rowId: docId,
+    });
+    let data = JSON.parse(doc.data);
+    if (typeof data === "string") {
+      data = JSON.parse(data);
+    }
+    return data;
+  } catch (error) {
+    console.log("Could not get data", error);
     throw error;
   }
 };
