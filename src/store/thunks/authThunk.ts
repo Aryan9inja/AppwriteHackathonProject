01f2f073d7d82763cbbb3d2b78e570b045434aa7
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-09-04 13:54:37 +0530

    Refactor auth thunks: Improve code formatting and enhance user data handling in login and logout processes

--- src/store/thunks/authThunk.ts
+++ src/store/thunks/authThunk.ts
@@ -1,6 +1,12 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import type { LoginFormData, SignUpFormData } from "@/types/types";
-import { registerUser, loginUser, logout,getUserData } from "@/services/users.services";
+import {
+  registerUser,
+  loginUser,
+  logout,
+  getUserData,
+} from "@/services/users.services";
+import type { RootState } from "../store";
 
 // Register user thunk
 export const registerUserThunk = createAsyncThunk(
@@ -24,6 +30,7 @@ export const loginUserThunk = createAsyncThunk(
     try {
       await loginUser(formData);
       const userData = await getUserData();
+      localStorage.setItem("userData", JSON.stringify(userData));
       return userData;
     } catch (error: unknown) {
       const err = error as { message?: string };
@@ -38,6 +45,7 @@ export const logoutUserThunk = createAsyncThunk(
   async (_, { rejectWithValue }) => {
     try {
       await logout();
+      localStorage.removeItem("userData")
       return null;
     } catch (error: unknown) {
       const err = error as { message?: string };
@@ -49,9 +57,14 @@ export const logoutUserThunk = createAsyncThunk(
 // Get user data thunk
 export const getUserDataThunk = createAsyncThunk(
   "auth/getUserData",
-  async (_, { rejectWithValue }) => {
+  async (_, { getState, rejectWithValue }) => {
+    const state = getState() as RootState;
+    if (state.auth.user) {
+      return state.auth; // Skip if already loaded
+    }
     try {
-      const user=await getUserData();
+      const user = await getUserData();
+      localStorage.setItem("userData", JSON.stringify(user));
       return user;
     } catch (error: unknown) {
       const err = error as { message?: string };
