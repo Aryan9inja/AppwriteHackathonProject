01d77ec7288dcac88b5a6273ccd3944e53b2838f
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-09-04 13:52:34 +0530

    Wrap App component with PersistingAuth for improved authentication handling

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import { store } from "./store/store";
 import "@/index.css";
 import App from "@/App";
-import { BrowserRouter } from "react-router-dom";
+import PersistingAuth from "@/components/auth/PersistingAuth";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistingAuth>
+          <App />
+        </PersistingAuth>
       </BrowserRouter>
     </Provider>
   </StrictMode>
