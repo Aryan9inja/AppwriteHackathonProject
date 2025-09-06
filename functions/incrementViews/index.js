13595ca14941b65bf9a0f2a54787935e77b9fd01
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-09-06 19:28:02 +0530

    Create increment views function

---
+++ functions/incrementViews/index.js
@@ -0,0 +1,65 @@
+const sdk = require("node-appwrite");
+
+module.exports = async ({ req, res, log, error }) => {
+  try {
+    const client = new sdk.Client()
+      .setEndpoint(process.env.APPWRITE_ENDPOINT)
+      .setProject(process.env.APPWRITE_PROJECT_ID)
+      .setKey(process.env.APPWRITE_API_KEY);
+
+    const tables = new sdk.TablesDB(client);
+
+    const portfolioId = req.query.portfolioId || req.body.portfolioId;
+    const ip = req.headers["x-forwarded-for"] || req.ip;
+
+    if (!portfolioId) {
+      return res.json({ error: "Missing portfolioId" }, 400);
+    }
+
+    const twentyFourHoursAgo = new Date(
+      Date.now() - 24 * 60 * 60 * 1000
+    ).toISOString();
+
+    const existing = await tables.listRows({
+      databaseId: process.env.DATABASE_ID,
+      tableId: process.env.PORTFOLIO_VIEWS_TABLE_ID,
+      queries: [
+        sdk.Query.equal("portfolioId", portfolioId),
+        sdk.Query.equal("ip", ip),
+        sdk.Query.greaterThan("timestamp", twentyFourHoursAgo),
+      ],
+    });
+
+    if (existing.total > 0) {
+      return res.json({ message: "View already counted", viewsUpdated: false });
+    }
+
+    await tables.createRow({
+      databaseId: process.env.DATABASE_ID,
+      tableId: process.env.PORTFOLIO_VIEWS_TABLE_ID,
+      data: {
+        portfolioId,
+        ip,
+        timestamp: new Date().toISOString,
+      },
+    });
+
+    const portfolio = await tables.getRow({
+      databaseId: DATABASE_ID,
+      tableId: process.env.PORTFOLIO_TABLE_ID,
+      rowId: portfolioId,
+    });
+    await tables.updateRow({
+      databaseId: DATABASE_ID,
+      tableId: process.env.PORTFOLIO_TABLE_ID,
+      rowId: portfolioId,
+      data: {
+        views: portfolio.views + 1,
+      },
+    });
+
+    return res.json({ message: "View counted", viewsUpdated: true });
+  } catch (error) {
+    return res.json({ error: err.message }, 500);
+  }
+};
