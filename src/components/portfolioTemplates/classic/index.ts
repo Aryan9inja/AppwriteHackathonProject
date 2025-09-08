0690395ff043a08493b24402481d916cce7c68a2
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-09-09 00:14:28 +0530

    Create index file for easy exports

---
+++ src/components/portfolioTemplates/classic/index.ts
@@ -0,0 +1,8 @@
+// Export all the classic template components
+export { default as HomePage } from './HomePage';
+export { default as AboutPage } from './AboutPage';
+export { default as ExperiencePage } from './ExperiencePage';
+export { default as EducationPage } from './EducationPage';
+export { default as ProjectsPage } from './ProjectsPage';
+export { default as SharedLayout } from './SharedLayout';
+export { default as ClassicPortfolioWebsite } from './ClassicPortfolioWebsite';
